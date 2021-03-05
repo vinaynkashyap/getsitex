f3e4256394109991c7214f519043551e5bf4e929
vinaynkashyap <vinaynkashyap@gmail.com>
2021-03-05 23:20:52 +0530

    cards

--- dist/redx/js/app.js
+++ dist/redx/js/app.js
@@ -15,7 +15,6 @@
 // -----------------------------------------------
 
 
-
 let navMain = document.getElementById('main-menu')
 let toggleButtonPrimary = document.getElementById('toggle-btn-primary')
 toggleButtonPrimary.addEventListener('click', () => {
@@ -35,19 +34,44 @@ toggleButtonPrimary.addEventListener('click', () => {
 // Slider 2 - Fullscreen slider
 // -----------------------------------------------
 
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 'auto',
-  centeredSlides: true,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-});
+// const swiper = new Swiper('.swiper-container', {
+//   slidesPerView: 'auto',
+//   centeredSlides: true,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev"
+//   },
+// });
+
+// -----------------------------------------------
+// Tabs
+// -----------------------------------------------
+
+
+
+const tabs = document.querySelectorAll('[data-tab]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = document.querySelector(tab.dataset.tab)
+
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
 
 
 // -----------------------------------------------
